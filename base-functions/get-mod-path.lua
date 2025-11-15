5e9281db70c0670a4ce6fcbe47cfe8a08394d142
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-11-15 13:57:38 -0300

    updated bugs

--- base-functions/get-mod-path.lua
+++ base-functions/get-mod-path.lua
@@ -1,20 +1,19 @@
---- Utilitário para detectar automaticamente o nome do mod atual
---- Funciona exclusivamente no data stage (data.lua, data-updates.lua, data-final-fixes.lua)
---- Uso:
----     local path_main = require("base-functions.get-mod-path")()
----
---- @return string Caminho formatado do mod atual, ex: "__LDA-LIB__/"
+-- get-mod-path.lua
+-- @return string Caminho formatado do mod atual, ex: "__LDA-LIB__/"
 
 local function getModPath()
-    -- Obtém o path interno enviado pelo Factorio ao carregar o módulo
-    local current_path = (... or "")
+    local info = debug.getinfo(2, "S")
 
-    -- Extrai o nome do mod do padrão "__MOD__/subfolder/file.lua"
-    --- @type string|nil
-    local mod_name = current_path:match("__([^/]+)__")
+    if not info or not info.source then
+        error("get-mod-path.lua: não foi possível obter o source do módulo.")
+    end
+
+    -- Exemplo de info.source:
+    -- "@__LDA-LIB__/base-functions/get-mod-path.lua"
+    local mod_name = info.source:match("@__([^/]+)__")
 
     if not mod_name then
-        error("get-mod-path.lua: Não foi possível detectar o nome do mod automaticamente.")
+        error("get-mod-path.lua: não foi possível detectar o nome do mod a partir de debug.getinfo.")
     end
 
     return "__" .. mod_name .. "__/"
--- init.lua
+++ init.lua
@@ -7,24 +7,27 @@ local LDA = {}
 ---------------------------------------------------------------------
 -- IMPORTAÇÕES DOS MÓDULOS
 ---------------------------------------------------------------------
+-- BASE FUNCTIONS
+local CO   = require("__LDA-LIB__/base-functions/create-ore")
+local CI   = require("__LDA-LIB__/base-functions/create-item")
+local CBI  = require("__LDA-LIB__/base-functions/create-block-item")
+local CF   = require("__LDA-LIB__/base-functions/create-fluid")
+local CG   = require("__LDA-LIB__/base-functions/create-gas")
+local CR   = require("__LDA-LIB__/base-functions/create-recipe")
 
-local CO = require("__LDA-LIB__/generic-functions/create-ore")
-local CI = require("__LDA-LIB__/generic-functions/create-item")
-local CBI = require("__LDA-LIB__/generic-functions/create-block-item")
+-- GENERIC FUNCTIONS
+local CIR  = require("__LDA-LIB__/generic-functions/create-item-with-recipe")
+local CFR  = require("__LDA-LIB__/generic-functions/create-fluid-with-recipe")
 local CBIR = require("__LDA-LIB__/generic-functions/create-block-item-with-recipe")
-local CF = require("__LDA-LIB__/generic-functions/create-fluid")
-local CG = require("__LDA-LIB__/generic-functions/create-gas")
-local CR = require("__LDA-LIB__/generic-functions/create-recipe")
-local CIR = require("__LDA-LIB__/generic-functions/create-item-with-recipe")
-local CFR = require("__LDA-LIB__/generic-functions/create-fluid-with-recipe")
-local CGR = require("__LDA-LIB__/generic-functions/create-generic-recipe")
+local CGR  = require("__LDA-LIB__/generic-functions/create-generic-recipe")
 
--- funções avançadas
+-- ADVANCED FUNCTIONS
 local CSIR = require("__LDA-LIB__/functions/create-smelting-item-with-recipe")
-local CIRM = require("__LDA-LIB__/functions/create-item-with-recipe-matrix")
+local CIRM = require("__LDA-LIB__/functions/create-item-with-recipe-matrix")   -- SE EXISTIR
 local CAIR = require("__LDA-LIB__/functions/create-assembler-item-with-recipe")
 local CPCIR = require("__LDA-LIB__/functions/create-particle-collider-item-with-recipe")
 local TECH = require("__LDA-LIB__/functions/create-technology")
+
 
 ---------------------------------------------------------------------
 -- API PÚBLICA
@@ -41,9 +44,7 @@ local TECH = require("__LDA-LIB__/functions/create-technology")
 --- @field createFluidWithRecipe fun(...) Fluido + receita.
 --- @field createBlockItemWithRecipe fun(...) Bloco + receita.
 --- @field createSmeltingItemWithRecipe fun(...) Item especial de fundição.
---- @field createItemWithRecipeMatrix fun(...) Matrizes de receitas complexas.
 --- @field createAssemblerItemWithRecipe fun(...) Máquina montadora customizada.
---- @field createParticleColiderItemWithRecipe fun(...) Item + receita para colisor.
 --- @field createTechnology fun(...) Cria tecnologias completas.
 --- @field createGenericRecipe fun(...) Cria receita genérica flexível.
 
@@ -58,9 +59,7 @@ LDA.functions = {
     createFluidWithRecipe = CFR.createFluidWithRecipe,
     createBlockItemWithRecipe = CBIR.createBlockItemWithRecipe,
     createSmeltingItemWithRecipe = CSIR.createSmeltingItemWithRecipe,
-    createItemWithRecipeMatrix = CIRM.createItemWithRecipeMatrix,
     createAssemblerItemWithRecipe = CAIR.createAssemblerItemWithRecipe,
-    createParticleColiderItemWithRecipe = CPCIR.createParticleColiderItemWithRecipe,
     createTechnology = TECH.createTechnology,
     createGenericRecipe = CGR.createGenericRecipe
 }
