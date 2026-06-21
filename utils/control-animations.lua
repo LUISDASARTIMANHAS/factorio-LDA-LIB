f3a6dccb7c2c58af16931b0ee49484b985af8208
HELLOKITTY-I5-L <luisaugustodesouza785@gmail.com>
2026-06-21 12:52:07 -0300

    fixed bugs

--- utils/control-animations.lua
+++ utils/control-animations.lua
@@ -194,12 +194,41 @@ function Module.createGraphicsSet(
     return {
         animation_progress = animation_progress or 0.25,
         always_draw_idle_animation = always_draw_idle_animation ~= false,
-        frozen_patch = frozen_patch or
-            util.sprite_load(
-                base_filename or "__space-age__/graphics/entity/electromagnetic-plant/electromagnetic-plant-frozen",
-                {scale = 0.5}
-            ),
-        water_reflection = Module.createWaterReflection(base_filename or "__base__/graphics/icons/coal"),
+        frozen_patch = {
+            north = {
+                filename = "__base__/graphics/entity/assembling-machine-3/assembling-machine-3-pipe-N.png",
+                priority = "extra-high",
+                width = 71,
+                height = 38,
+                shift = util.by_pixel(2.25, 13.5),
+                scale = 0.5
+            },
+            east = {
+                filename = "__base__/graphics/entity/assembling-machine-3/assembling-machine-3-pipe-E.png",
+                priority = "extra-high",
+                width = 42,
+                height = 76,
+                shift = util.by_pixel(-24.5, 1),
+                scale = 0.5
+            },
+            south = {
+                filename = "__base__/graphics/entity/assembling-machine-3/assembling-machine-3-pipe-S.png",
+                priority = "extra-high",
+                width = 88,
+                height = 61,
+                shift = util.by_pixel(0, -31.25),
+                scale = 0.5
+            },
+            west = {
+                filename = "__base__/graphics/entity/assembling-machine-3/assembling-machine-3-pipe-W.png",
+                priority = "extra-high",
+                width = 39,
+                height = 73,
+                shift = util.by_pixel(25.75, 1.25),
+                scale = 0.5
+            }
+        },
+        water_reflection = Module.createWaterReflection(base_filename),
         reset_animation_when_frozen = reset_animation_when_frozen ~= false
     }
 
